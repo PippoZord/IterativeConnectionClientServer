5fd7a4cfbcf5c774d2af6d07b9f2668048f61d86
Lorenzo Filipponi <pippolor3@gmail.com>
2022-07-23 09:20:55 +0200

    Prova di commit

---
+++ Client.java
@@ -0,0 +1,96 @@
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.nio.Buffer;
+import java.util.stream.Stream;
+
+import javax.xml.stream.util.StreamReaderDelegate;
+
+public class Client {
+    public static void main(String[] args) {
+        InetAddress ia;
+        InetSocketAddress isa;
+        InetSocketAddress Myisa;
+        Socket sClient;
+        String msg;
+        sClient = new Socket();
+        
+        try {
+            if (args.length != 2) throw new IllegalArgumentException("PARAMETRI ERRATI");
+            ia = InetAddress.getLocalHost();
+            Myisa = new InetSocketAddress(ia, 0);
+            sClient.bind(Myisa);
+            int port = Integer.parseInt(args[1]);
+            isa = new InetSocketAddress(args[0], port);
+            sClient.connect(isa);
+            
+            do {
+                msg = readFromServer(sClient);
+                System.out.println(msg);
+                msg = read();
+                sendToServer(msg, sClient);
+
+                msg = readFromServer(sClient);
+                System.out.println(msg);
+                if (!msg.equals("OPZIONE NON VALIDA")){
+                    msg = read();
+                    sendToServer(msg, sClient);
+                
+                    msg = readFromServer(sClient);
+                    System.out.println(msg);
+                }
+                
+                sendToServer("Y", sClient);
+            
+                do {
+                    msg = readFromServer(sClient);
+                    System.out.println(msg);
+                    msg = read();
+                    sendToServer(msg, sClient);
+                } while (!msg.equals("N") && !msg.equals("Y"));
+
+            } while (msg.equals("Y"));
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 
+     * @return Stringa letta tramite System.in
+     * @throws IOException
+     */
+    private static String read() throws IOException {
+        InputStreamReader in = new InputStreamReader(System.in);
+        BufferedReader bu = new BufferedReader(in);
+        String s = bu.readLine();
+        return s;
+    }
+
+    /**
+     * Invia msg al Server
+     * 
+     * @throws IOException
+     */
+    private static void sendToServer(String msg, Socket sClient) throws IOException {
+        OutputStream ou = sClient.getOutputStream();
+        ou.write(msg.getBytes(), 0, msg.getBytes().length);
+    }
+
+    /**
+     * @return legge dal Server
+     * @throws IOException
+     */
+    private static String readFromServer(Socket sClient) throws IOException {
+        InputStream in = sClient.getInputStream();
+        byte[] buf = new byte[128];
+        int l = in.read(buf);
+        return new String(buf, 0, l);
+    }
+}
---
+++ Server.java
@@ -0,0 +1,116 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.StringTokenizer;
+
+import javax.swing.InputMap;
+
+public class Server {
+    public static void main(String[] args) {
+        ServerSocket srv;
+        Socket sClient;
+        String msg;
+        
+        sClient = new Socket();
+        
+        try {
+            srv = new ServerSocket(0);
+            System.out.println("INDIRIZZO: " + srv.getLocalPort() + "\tPORTA: " + srv.getLocalPort());
+            
+            while (true) {
+                sClient = srv.accept();
+                boolean l=true;
+                do {
+                
+                    sendToClient("SCEGLI LA FIGURA: ", sClient);
+                    msg = readFromClient(sClient);
+
+                    switch (msg) {
+                        case "quadrato":
+                            System.out.println(msg);
+                            sendToClient("INSERISCI IL LATO DEL QUADRATO", sClient);
+                            msg = readFromClient(sClient);
+                            double tmp = Double.parseDouble(msg);
+                            System.out.println(tmp);
+                            sendToClient("QUADRATO\tAREA = " + tmp*tmp +"\tPERIMETRO = " + tmp*4, sClient);
+                            break;
+                        case "cerchio":
+                            System.out.println(msg);
+                            sendToClient("INSERISCI IL RAGGIO DEL CERCHIO", sClient);
+                            msg = readFromClient(sClient);
+                            tmp = Double.parseDouble(msg);
+                            System.out.println(tmp);
+                            sendToClient("CERCHIO\tAREA = " + Math.PI * (tmp*tmp) + "\tCIRCONFERENZA = " + 2*Math.PI*tmp, sClient);
+                            break;
+                        case "rettangolo":
+                            System.out.println(msg);
+                            sendToClient("INSERISCI I LATI DEL RETTANGOLO", sClient);
+                            msg = readFromClient(sClient);
+                            StringTokenizer tokenString = new StringTokenizer(msg);
+                            tmp = Double.parseDouble(tokenString.nextToken());
+                            double tmp2 = Double.parseDouble(tokenString.nextToken());
+                            System.out.println(tmp  +" " + tmp2);
+                            sendToClient("RETTANGOLO\tAREA = " + tmp * tmp2 + "\tPERIMETRO = "
+                                    + (tmp + tmp + tmp2 + tmp2), sClient);
+                            break;
+                        case "triangolo":
+                            System.out.println(msg);
+                            sendToClient("INSERISCI I CATETI DEL RETTANGOLO", sClient);
+                            msg = readFromClient(sClient);
+                            tokenString = new StringTokenizer(msg);
+                            tmp = Double.parseDouble(tokenString.nextToken());
+                            tmp2 = Double.parseDouble(tokenString.nextToken());
+                            double ipo = Math.sqrt((tmp*tmp + tmp2*tmp2));
+                            System.out.println(tmp + " " + tmp2 + " " + ipo);
+                            sendToClient("TRIANGOLO RETTANGOLO\tAREA = " + tmp * tmp2 + "\tPERIMETRO = " + (tmp + tmp2 + ipo), sClient);
+                            break;
+                        default:
+                            sendToClient("OPZIONE NON VALIDA", sClient);
+                            break;
+                    }
+                    
+                    readFromClient(sClient);
+
+                    do {
+                        sendToClient("VUOI CONTINUARE? (Y/N)", sClient);
+                        msg = readFromClient(sClient);
+                    } while (!msg.equals("N") && !msg.equals("Y"));
+
+                    if (msg.equals("N")){
+                        l = false;
+                    }
+                } while (l);
+            }
+
+
+
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Invia msg al Client
+     * 
+     * @throws IOException
+     */
+    private static void sendToClient(String msg, Socket sClient) throws IOException{
+        OutputStream ou = sClient.getOutputStream();
+        ou.write(msg.getBytes(), 0, msg.getBytes().length);
+    }
+
+    /**
+     * @return Legge dal Client
+     * @throws IOException
+     */
+    private static String readFromClient(Socket sClient) throws IOException{        
+        InputStream in = sClient.getInputStream();
+        byte[] buf= new byte[128];
+        int l = in.read(buf);
+        return new String(buf, 0, l);
+    }
+}
